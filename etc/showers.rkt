565aafcfa063b701aa1872408f1b22c90d7ec67e
Ed Kademan <ed@kademan.org>
2018-03-01 11:45:31 -0500

    wip showers

--- etc/showers.rkt
+++ etc/showers.rkt
@@ -43,21 +43,21 @@
            ( "5:30pm" .  "6:30pm")))
     (job3 (( "9:30pm" . "10:30pm")))))
 
-;; (define showers
-;;   '(("Shower SEB 1" . 1)
-;;     ("Shower SEB 2" . 1)
-;;     ("Shower SEB 3" . 1)))
-
-;; (define shower-times
-;;   '(( "8:30am" . "8:50am")
-;;     ( "8:50am" . "9:10am")
-;;     ("12:50pm" . "1:10pm")))
-
 (define showers
-  '(("Shower SEB 1" . 1)))
+  '(("Shower SEB 1" . 1)
+    ("Shower SEB 2" . 1)
+    ("Shower SEB 3" . 1)))
 
 (define shower-times
-  '(("12:50pm" . "1:10pm")))
+  '(( "8:30am" . "8:50am")
+    ( "8:50am" . "9:10am")
+    ("12:50pm" . "1:10pm")))
+
+;; (define showers
+;;   '(("Shower SEB 1" . 1)))
+
+;; (define shower-times
+;;   '(("12:50pm" . "1:10pm")))
 
 (define roster
   '(("Fred Flintstone"          job1)
@@ -65,8 +65,57 @@
     ("Fabian Snodgrass"         job3)))
 
 ;;; * showers
-;;; Assign showers and shower times to the individuals that do not
-;;; interfere with their jobs.
+;;;
+;;; An assig is a location-time combination. For example
+;;;   ("Shower SEB 1" . ("8:30am" . "8:50am"))
+;;; and assigs are a list of such pairs:
+;;;   '(("Shower SEB 1" . (" 8:30am" . "8:50am"))
+;;;     ("Shower SEB 1" . (" 8:50am" . "9:10am"))
+;;;     ("Shower SEB 1" . ("12:50pm" . "1:10pm")))
+
+(define all-assigs
+  (apply append
+         (map (lambda (s) (map (lambda (t) (cons (car s) t))
+                          shower-times))
+              showers)))
+
+;;; update assigs for individual w/all-assigs
+;;; if all-assigs is empty error
+;;; for each assig in all-assigs
+;;;   if appropriate for individual (jobtimes & capacity)
+;;;     update assigs with assig
+;;;     return assigs
+;;; ;nothing appropriate
+;;; remove previous assig from assigs and remove temporarily from
+;;; all-assigs
+;;; now update assigs for previous individual w/temp all-assigs
+;;; restore all-assigs to original state and redo
+;;;
+;;;
+;;;
+;;; start out with assigs empty
+;;; for each individual
+;;; 
+
+
+
+
+
+(define past '())
+
+(define (note x)
+  (call-with-composable-continuation
+   (lambda (k)
+     (set! past (cons k past))
+     x)))
+
+
+
+
+
+
+
+
 
 (define (job-for name) (cadr (assoc name roster)))
 (define (job-times-for job [jtimes jobs])
@@ -74,86 +123,3 @@
    ((null? jtimes) #f)
    ((eq? job (caar jtimes)) (cadar jtimes))
    (else (job-times-for job (cdr jtimes)))))
-
-;;; Find a shower time for name from among shower-times.
-(define (shower-time-for name shower-times)
-  (cond
-   ((null? shower-times) #f)
-   ((time-collision? (car shower-times)
-                     (job-times-for (job-for name)))
-    (shower-time-for name (cdr shower-times)))
-   (else (car shower-times))))
-
-;;; Return the number of people using the shower at the shower time.
-(define (n-shower-users shower shower-time shower-assignments)
-  (define (match? shower-assignment)
-    (and (string=? shower (cadr shower-assignment))
-         (equal? shower-time (caddr shower-assignment))))
-  (length (filter match? shower-assignments)))
-
-(define (shower-capacity shower) (cdr (assoc shower showers)))
-
-;;; Allocate a shower at time shower-time from among showers.
-(define (find-shower shower-time showers shower-assignments)
-  (if (null? showers)
-      #f
-      (let ((shower  (caar showers))
-            (showers (cdr showers)))
-        (if (< (n-shower-users shower shower-time shower-assignments)
-               (shower-capacity shower))
-            shower
-            (find-shower shower-time showers shower-assignments)))))
-
-;;; for an individual
-;;; for each possible shower time
-;;;   for each shower
-;;;     if time/shower is available save to assign
-;;; if no time/shower available
-;;;   remove the shower used by the previous individual and redo the
-;;;   assignment for that individual
-;;;
-
-;;; (REDO) means find the shower for the previous assignment and
-;;; remove it from showers, and remove the previous assignment from
-;;; assign and do over for the previous individual. Then, when you get
-;;; another set of assignments, restore the shower and do over for the
-;;; current individual.
-
-(define (reassign-previous-shower/time showers times assign)
-  (match-let ([(list name shower time) (car assign)])
-    (let* ((shs (remove shower showers))
-           (assign (assign-a-shower/time
-                    name shs times (cdr assign))))
-      assign)))
-
-(define (assign-a-shower/time name showers times assign)
-  (and (null? showers) (null? times) (error 'impossible))
-  (let ((st (shower-time-for name times)))
-    (cond
-     ((not st)
-      (assign-a-shower/time
-       name showers times
-       (reassign-previous-shower/time showers times assign)))
-     (else
-      (let ((sh (find-shower st showers assign)))
-        (if (not sh)
-            (assign-a-shower/time
-             name showers times
-             (reassign-previous-shower/time showers times assign))
-            (cons (list name sh st) assign)))))))
-
-(define (create-shower-assignments)
-  (let loop ((names (map car roster))
-             (showers showers)
-             (times shower-times)
-             (assign '()))  ;(e ...), e is (name shower time)
-    (if (null? names)
-        assign
-        (loop (cdr names)
-              showers times
-              (call-with-composable-continuation
-               (lambda (k)
-                 
-                 ))
-              (assign-a-shower/time
-               (car names) showers times assign)))))
